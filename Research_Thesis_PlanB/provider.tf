219fc46097bbad6f61630fb737b772414ceabc29
ananthmkashyap <131719080+ananthmkashyap@users.noreply.github.com>
2023-12-31 16:08:42 +0100

    Delete Research_Thesis_PlanB directory

--- Research_Thesis_PlanB/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-    region                   = "eu-central-1"
-    shared_config_files      = ["C:/Users/Admin/.aws/config"]
-    shared_credentials_files = ["C:/Users/Admin/.aws/credentials"]
-    profile                  = "default"
-}
