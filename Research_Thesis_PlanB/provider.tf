0cb1fde863d44a0422441149c0e657f08f0375ba
ananthmkashyap <ananth.m.kashyap@gmail.com>
2023-12-31 11:53:53 +0100

    New Folder

---
+++ Research_Thesis_PlanB/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    region                   = "eu-central-1"
+    shared_config_files      = ["C:/Users/Admin/.aws/config"]
+    shared_credentials_files = ["C:/Users/Admin/.aws/credentials"]
+    profile                  = "default"
+}
